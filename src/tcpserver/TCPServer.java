bae359eb1bd23d9d12b5b647195e1f6ebd9aff67
Davi Pereira Neto <davineto@alunos.utfpr.edu.br>
2017-08-16 16:08:09 -0300

    TCPServer criação

---
+++ src/tcpserver/TCPServer.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcpserver;
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author davi
+ */
+public class TCPServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                try{
+			int serverPort = 7896; // the server port
+			ServerSocket listenSocket = new ServerSocket(serverPort);
+			while(true) {
+				Socket clientSocket = listenSocket.accept();
+				Connection c = new Connection(clientSocket);
+			}
+		} catch(IOException e) {System.out.println("Listen socket:"+e.getMessage());}    }
+    
+}
+
+class Connection extends Thread {
+	DataInputStream in;
+	DataOutputStream out;
+	Socket clientSocket;
+	public Connection (Socket aClientSocket) {
+		try {
+			clientSocket = aClientSocket;
+			in = new DataInputStream( clientSocket.getInputStream());
+			out =new DataOutputStream( clientSocket.getOutputStream());
+			this.start();
+		} catch(IOException e) {System.out.println("Connection:"+e.getMessage());}
+	}
+        @Override
+	public void run(){
+		try {			                 // an echo server
+
+			String data = in.readUTF();	                  // read a line of data from the stream
+			out.writeUTF(data);
+		}catch (EOFException e){System.out.println("EOF:"+e.getMessage());
+		} catch(IOException e) {System.out.println("readline:"+e.getMessage());
+		} finally{ try {clientSocket.close();}catch (IOException e){/*close failed*/}}
+		
+
+	}
+}
